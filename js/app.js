978fe78b7b9ed4bc01e95ae4a3dab513deb3a2fe
Masahira <jacopas69@gmail.com>
2020-03-16 23:14:36 +0900

    モーダル表示部分の見直し
    クリックされた箇所によってモーダルの中身を動的に変更する。

--- js/app.js
+++ js/app.js
@@ -18,53 +18,80 @@ oderW.color = ['white'];
 oderW.point = [0,0,0,0,0,];
 
 let modal = document.getElementById('modal');
-let closeBtn = document.getElementById('closeBtn');
-let orderInput = document.getElementById('orderInput');
-let orderRegister = document.getElementById('orderRegister');
 
 //オーダー名クリックからの登録関数
 function getId(id_value){
   //モーダル表示
   modal.style.display = 'block';
-
-  //クラス登録処理
   let id = id_value.id;
-  //id名からナンバーだけ取り出し
-  let res = id.replace(/[^0-9]/g, '');
+  let bar = String(id.match(/oder/));
 
-  //クリックされた対象のvalueを取得しtextboxのvalueに指定。
-  let orderName = document.getElementById(id);
-  let inputValue = orderName.getAttribute("value");
-  orderInput.value = inputValue;
+  if(bar == 'oder'){
+    let modalBody = document.getElementById('modal-body');
+    modalBody.innerHTML = '<h2>選手名を登録してください。</h2>'+
+                          '<form>'+
+                          '<input id="orderInput" type="text" value="aa">'+
+                          '<input id="orderRegister" type="button" value="登録">'+
+                          '</form>'+
+                          '<a id="closeBtn" value="close">閉じる</a>';
+    //クリック対象のオーダーidを取得
+    let orderElement = document.getElementById(id);
+    //クリックされた対象のvalueを取得しtextboxのvalueに指定。
+    let orderInput = document.getElementById('orderInput');
+    let inputValue = orderElement.getAttribute("value");
+    orderInput.value = inputValue;
+
+    //id名からナンバーだけ取り出し
+    let res = id.replace(/[^0-9]/g, '');
+    //登録イベントへidの要素とidナンバー部分を引数に渡す。
+    orderRegistCall(orderElement,res);
+
+  }else
+    if(id == 'theams'){
+      let modalBody = document.getElementById('modal-body');
+      modalBody.innerHTML = "書き換えたいなあ～";
+    }
   
-  //テキストボックスのオーダー名に登録ボタンで書き換え
-  orderRegister.addEventListener('click', orderRegisterEvn);
-  function orderRegisterEvn(){
-    console.log(orderName);
-    orderName.innerHTML = orderInput.value;
-    //クラスnameに登録
-    if(res<5){
-      oderR.name[res] = orderInput.value;
-    }else{
-      res = res-5;
-      oderW.name[res] = orderInput.value;
-   }
-    console.log('オーダー１'+oderR);
-    console.log('オーダー2'+oderW);
-    //モーダルを閉じる
-    modal.style.display = 'none';
-    //イベントリスナ削除
-    orderRegister.removeEventListener('click', orderRegisterEvn);
-  }
 }
 
-//閉じるボタンでモーダルを閉じる
-closeBtn.addEventListener('click', function() {
-  modal.style.display = 'none';
-});
+
+
+
+
 //ウィンドウクリックでモーダルを閉じる
 window.addEventListener('click', function(e){
     if(e.target == modal){
         modal.style.display = 'none';
     }
 });
+
+
+
+
+function orderRegistCall(orderElement,res){
+  //テキストボックスのオーダー名に登録ボタンで書き換えイベント実行
+  orderRegister.addEventListener('click', orderRegisterEvn);
+  function orderRegisterEvn(){
+    //valueとHtmlの書き換え
+    orderElement.setAttribute("value",orderInput.value);
+    orderElement.innerHTML = orderInput.value;
+    //クラスnameに登録。オーダーの数によってoderRとoderWに分ける。
+    if(res<5){
+      oderR.name[res] = orderInput.value;
+    }else{
+      //オーダー数が5以上oderWにname追加
+      res = res-5;
+      oderW.name[res] = orderInput.value;
+  }
+    //モーダルを閉じる
+    modal.style.display = 'none';
+    //イベントリスナ削除
+    orderRegister.removeEventListener('click', orderRegisterEvn);
+  }
+
+  //閉じるボタンでモーダルを閉じる
+  let closeBtn = document.getElementById('closeBtn');
+  closeBtn.addEventListener('click', function() {
+  modal.style.display = 'none';
+});
+}
